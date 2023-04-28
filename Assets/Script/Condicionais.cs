feeae8ff9fbdf5cc35124e1bbfc04ec0abd56c9f
HanzoDeath666 <thalysonriuan@gmail.com>
2023-04-28 15:58:42 -0300

    aula 28/04/2023

--- Assets/Script/Condicionais.cs
+++ Assets/Script/Condicionais.cs
@@ -10,10 +10,10 @@ public class Condicionais : MonoBehaviour
     [SerializeField] int _i1;
     [SerializeField] int _i2;
 
-    string _t1;
+    [SerializeField] string _t1;
     string _t2;
 
-    bool _b1;
+    [SerializeField] bool _b1;
     bool _b2;
 
     // > Maior
@@ -22,24 +22,59 @@ public class Condicionais : MonoBehaviour
     // <= Menor ou Igual
     // != Diferente
     // == Igual
+    // && e
+    // || ou
 
     // Start is called before the first frame update
     void Start()
+       
     {
-        if(_i1==1) {
-            Debug.Log("_i1 é igual a 1");
+
+        if(_i1==1)
+        {
+            Debug.Log("Fase 1 liberada");
+        }
+        else
+        {
+            Debug.Log("Fase 1 bloqueado");
         }
 
-        if (_i1==_i2) {
-            Debug.Log("_i1 é igual a _i2");
 
+        if(_t1== "multiplicação")
+        {
+            Debug.Log(_i1 * _i2);
+           
+        }
+        else
+        {
+            Debug.Log(_i1 + _i2);
         }
 
+
+
+        if(_i1==2)
+        {
+            _t1 = "Passagem liberada";
+       
+        }
+
+        else
+        {
+            _t1 = "Passagem Bloqueada";
+        }
+        Debug.Log(_t1);
+
+        if (_i1 == _i2 && _b1 == true)
+        {
+            Debug.Log("Entrar na Fase");
+        }
     }
 
-    // Update is called once per frame
+
+
+
     void Update()
     {
-        
+
     }
 }
